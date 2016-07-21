908fe85662dad926908173eee5b87934616ce292
martidor <martin2609@hotmail.com>
2016-07-21 21:24:23 +0200

    notesProgram (edit/save and remove notes)

---
+++ src/jsxstuff.js
@@ -0,0 +1,7 @@
+var Bacon = React.createClass({
+    render: function() {
+        return (<h2>This is a simple component</h2>
+    }
+});
+
+ReactDOM.render(<Bacon/>, document.getElementById("example"));
